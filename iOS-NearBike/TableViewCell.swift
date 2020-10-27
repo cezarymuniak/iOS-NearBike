4155a4da5cc8b9fcf831ea88dfbd5bf7e4807992
Cezary Muniak <>
2020-10-27 14:45:41 +0100

    created table view cell

---
+++ iOS-NearBike/TableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TableViewCell.swift
+//  iOS-NearBike
+//
+//  Created by CM on 27/10/2020.
+//
+
+import Foundation
+import UIKit
+class TableViewCell: UIView {
+    
+}
--- iOS-NearBike/ViewController.swift
+++ iOS-NearBike/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let tview = UINib(nibName: "TableViewCell", bundle: nil).instantiate(withOwner: nil, options: nil).first as! TableViewCell
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBOutlet weak var testView: TableViewCell!
+    
 }
 
